0a8682fb43826ccad0d5f0d9c284a7870629a671
Rodrigo Guerra <rodrigo.guerra@sistemasexpertos.cl>
2017-08-26 08:40:29 -0300

    cambio

--- cargar_usuario_webservice.php
+++ cargar_usuario_webservice.php
@@ -1,25 +1,12 @@
 <? 
 
-$xml='<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:cer="http://certificadorprevisional.fonasa.gov.cl.ws/">
-<soapenv:Header/>
-<soapenv:Body>
-<cer:getCertificadoPrevisional>
-<cer:query>
-<cer:queryTO>
-<cer:tipoEmisor>10</cer:tipoEmisor>
-<cer:tipoUsuario>1</cer:tipoUsuario>
-</cer:queryTO>
-<cer:entidad>61608000</cer:entidad>
-<cer:claveEntidad>6160</cer:claveEntidad>
-<cer:rutBeneficiario>'.$rut.'</cer:rutBeneficiario>
-<!--Optional:-->
-<cer:dgvBeneficiario>'.$dv.'</cer:dgvBeneficiario>
-<cer:canal>10</cer:canal>
-</cer:query>
-</cer:getCertificadoPrevisional>
-</soapenv:Body>
-</soapenv:Envelope>';
+$rutdv = '16786278-0';
 
 
+//Con esta funcion consultaremos al webservice
+function cargar_usuario($rutdv){
 
+	$rutdv = $rutdv;
+
+}
 ?>
